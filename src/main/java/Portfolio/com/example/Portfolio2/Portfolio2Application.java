0f209a283bd1f03aec0ccca2709541fa4d0b8891
Mohit <mohitsingholi07@gmail.com>
2024-12-12 15:37:44 +0530

    initial commit

---
+++ src/main/java/Portfolio/com/example/Portfolio2/Portfolio2Application.java
@@ -0,0 +1,13 @@
+package Portfolio.com.example.Portfolio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portfolio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Portfolio2Application.class, args);
+	}
+
+}
